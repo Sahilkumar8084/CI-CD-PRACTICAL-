d630ff0ee7a295f829bf6ecf6f4d8054f062d96e
Sahilkumar8084 <sahilkumarrock8084@gmail.com>
2026-04-22 12:42:49 +0530

    modified with the Added Errors

--- test/test_operations.py
+++ test/test_operations.py
@@ -6,8 +6,8 @@ def test_add():
     assert add(45,5) ==50
     assert add(43,5) ==48
     assert add(3,45) ==48
-    assert add(34,5) ==39
+    assert add(34,5) ==38
     
-def test_sub():
+def test_sub(): #pytest ko rn karne ke liye hume aise gunction banane honge jinke naam test_ se hoo yaa koi folder ya files  end ho _test .py se aisa kuchh
     assert sub(5,3) ==2
     assert sub(8684,21) == 8663
